dc42829086541f4fa0a5393b7e62e20ccd63c87a
Curtis Schlak <realistschuckle@gmail.com>
2015-06-25 06:37:11 -0500

    Add a new file.

--- src/server/app.js
+++ src/server/app.js
@@ -1,1 +1,16 @@
-app.js
+let path = require('path');
+let root = path.join(__dirname, '..');
+
+class TestDrivenTraining {
+  constructor(app, window, menu) {
+    this._app = app;
+    this._window = window;
+    this._menu = menu;
+  }
+
+  start() {
+    this._window.loadUrl()
+  }
+}
+
+module.exports = TestDrivenTraining;
