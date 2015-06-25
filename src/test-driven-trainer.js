62a720823fa04c523dbcb64883337cc6295debfb
Curtis Schlak <realistschuckle@gmail.com>
2015-06-24 20:06:48 -0500

    Make a splash screen separate from the training screen.

--- src/test-driven-trainer.js
+++ src/test-driven-trainer.js
@@ -57,7 +57,8 @@ app.on('ready', function() {
 
   
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadUrl('file://' + __dirname + '/splash.html');
+  mainWindow.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
